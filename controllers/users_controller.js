8f49597894b265e87386d44c25dfe08bccad9774
Harshit <harshit.it.21@nitj.ac.in>
2023-06-01 12:38:49 +0530

    Making a view of 2nd file throught ejs

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,6 @@
 module.exports.profile = function(req,res){
-    res.end("<h1>Hurray you are in profile section </h1>");
+    return res.render('user',
+    {
+        title:"User controller"
+    })
 }
