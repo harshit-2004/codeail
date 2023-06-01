7610c27af16a4d205c57b90617e0c2ab8536440d
Harshit <harshit.it.21@nitj.ac.in>
2023-06-01 12:26:38 +0530

    Set up our view engine

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const port = "8000";
 //              Use express router 
 app.use('/',require('./routes'));
 
+//              Set up our view engine
+app.set('view engine','ejs');
+app.set('views','./views');
+
 app.listen(port,function(err){
     if(err)
     {
